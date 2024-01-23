1fca8a25c962923e0bcb4d7610729d5d856892bd
hud-sonq <hudson@koipond.xyz>
2024-01-23 09:40:37 -0500

    edit dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@
 export default defineNuxtConfig({
   modules: [
       ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate']}],
-<<<<<<< HEAD
-=======
-      'nuxt-server-utils',
-      '@nuxt/ui',
->>>>>>> fed95b108c7613661f6c8bf81d92232955fa1f1c
   ],
   imports: {
       dirs: ['stores'],
@@ -14,26 +9,17 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
       mongoURI: '',
-<<<<<<< HEAD
-=======
       discordWebhook: '',
       authSecret: '',
-      owSecret: '',
->>>>>>> fed95b108c7613661f6c8bf81d92232955fa1f1c
       public: {
           apiBase: '',
       }
   },
   css: ['~/assets/global.css'],
   app: {
-<<<<<<< HEAD
       baseURL: '/'
   },
   nitro: {
     plugins: ["~/server/index.ts"],
   }
-=======
-      baseURL: '/',
-  },
->>>>>>> fed95b108c7613661f6c8bf81d92232955fa1f1c
 })
